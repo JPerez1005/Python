982282c8ec870ba6fc9c3fdc4a8d80acb912a1b5
JPerez1005 <perezbuenojuliandavid@gmail.com>
2023-06-24 08:48:19 -0500

    "Se volvio a organizar unas carpetas como las de practicas en clase y de casa, y se hizo algun que otro ejercicio"

---
+++ C/C1/Practicas Homework/2Estructuras Repetitivas (for)/NumerosDivisibles.py
@@ -0,0 +1,6 @@
+"""Construya un programa que muestre los números divisibles de 3 y 7 entre 1 y 1000."""
+num=114
+
+for i in range(1,1000):
+    if i%114==0:
+        print(f'El numero divisible entre {num} es de: {i}')
