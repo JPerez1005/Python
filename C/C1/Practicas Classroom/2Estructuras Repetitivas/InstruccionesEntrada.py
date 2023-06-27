e7b562f701402ec9e114358b89d6f324ef99dcca
Julian <116776667+JPerez1005@users.noreply.github.com>
2023-06-27 07:22:15 -0500

    Delete C/C1/Practicas Classroom directory

--- C/C1/Practicas Classroom/2Estructuras Repetitivas/InstruccionesEntrada.py
+++
@@ -1,6 +0,0 @@
-var = input('ingrese su edad: ')
-var_converter=int(var)
-print(type(var_converter))
-
-temp=float(input('Digite la temperatura:'))
-print(type(temp))
--- C/C1/Practicas Classroom/2Estructuras Repetitivas/ejemplos.py
+++
@@ -1,8 +0,0 @@
-for i in range(5):
-    print(i, end=', ')#0,1,2,3,4,
-
-for i in range(-1,5):
-    print(i, end=', ')# -1,0,1,2,,3,4,
-
-for i in range(-1, 5, 2):
-    print(i, end=', ')# -1, 1, 3,
