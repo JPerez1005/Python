1725697287ca653281ae99c1bbd973dc27d1118e
JPerez1005 <perezbuenojuliandavid@gmail.com>
2023-06-30 12:45:12 -0500

    "otro archivo mas"

--- C/C1/Practicas Homework/5Funciones/PorcentajeIVA.py
+++ C/C1/Practicas Homework/5Funciones/PorcentajeIVA.py
@@ -4,3 +4,14 @@ debe recibir la cantidad sin IVA y el porcentaje
 de IVA a aplicar, y devolver el total de la
 factura. Si se invoca la función sin pasarle
 el porcentaje de IVA, deberá aplicar un 21%.'''
+
+def CalcularIva():
+    ValorSinIVA=int(input('Digite el Valor sin IVA: '))
+    ValorIVA=float(input('Digite el valor del porcentaje del iva: '))
+    if ValorIVA=='':
+        ValorIVA=21
+    PorcentajeIVA=ValorIVA/100
+    ValorTotal=PorcentajeIVA*ValorSinIVA
+    print('\n','-'*5,'FACTURA','-'*5,'\n')
+    print('El valor total de la factura es: ',ValorTotal)
+    print('\n','-'*5,'Fin','-'*5,'\n')
