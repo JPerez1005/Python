72d5a50cc3e2a07ad1a26f9bd84a8b32d1be9925
Julian <116776667+JPerez1005@users.noreply.github.com>
2023-07-06 21:19:54 -0500

    Add files via upload

---
+++ C/C1/Practicas Classroom/6Memorias/Diccionarios/emplo.py
@@ -0,0 +1,13 @@
+empls={5: {
+    'nombre':'carlos',
+    'hrs_trabajadas':5,
+    'vlr_hora':10
+    },
+    7: {
+        'nombre':None,
+        'hrs_trabajadas':None,
+        'vlr_hora':None
+    }
+}
+
+empls[5]['nombre']='Bartolomeo'
